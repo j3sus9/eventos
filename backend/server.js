1137a4517f337325e2421f9043fd28634821f0d5
j3sus9 <jesusrepisouma@gmail.com>
2025-12-10 15:32:48 +0100

    Fix backend dependencies and mongo URI

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
-const mongoURI = 'mongodb+srv://jesusrepisouma_db_user:jRexvSVo2afPFURl@examenfrontend.6tl8muv.mongodb.net/';
+const mongoURI = process.env.MONGO_URI || 'mongodb+srv://jesusrepisouma_db_user:jRexvSVo2afPFURl@examenfrontend.6tl8muv.mongodb.net/eventual?retryWrites=true&w=majority';
 
 mongoose.connect(mongoURI)
   .then(() => console.log('MongoDB connected'))
